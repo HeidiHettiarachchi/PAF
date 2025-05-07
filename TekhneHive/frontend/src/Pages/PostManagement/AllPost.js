101bf2e25dcffeed8ab75e34334cf809a541f04e
HeidiHettiarachchi <it22323248@my.sliit.lk>
2025-05-07 21:11:58 +0530

    Update: Button style

--- TekhneHive/frontend/src/Pages/PostManagement/AllPost.js
+++ TekhneHive/frontend/src/Pages/PostManagement/AllPost.js
@@ -9,6 +9,7 @@ import Modal from 'react-modal';
 import NavBar from '../../Components/NavBar/NavBar';
 import { IoIosCreate } from "react-icons/io";
 import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
 import { GrUpdate } from "react-icons/gr";
 import { FiSave } from "react-icons/fi";
 import { TbPencilCancel } from "react-icons/tb";
@@ -319,7 +320,7 @@ function AllPost() {
             </div>
 
           </button>
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{display: 'block', height: 0, width: 0 }}>
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'block', height: 0, width: 0 }}>
             <defs>
               <filter id="goo">
                 <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur"></feGaussianBlur>
@@ -376,8 +377,9 @@ function AllPost() {
                       <button className="edit-btn" onClick={() => handleUpdate(post.id)}>
                         <FaEdit size={18} />
                       </button>
+
                       <button className="delete-btn" onClick={() => handleDelete(post.id)}>
-                        <RiDeleteBin6Fill size={18} />
+                        <MdDelete size={18} />
                       </button>
                     </div>
                   )}
@@ -462,28 +464,44 @@ function AllPost() {
                             <>
                               {editingComment.id === comment.id ? (
                                 <>
-                                  <button
+                                  <button className="save-btn-comment"
                                     onClick={() =>
                                       handleSaveComment(post.id, comment.id, editingComment.content)
                                     }
                                   >
-                                    <FiSave />
+                                    {/* <FiSave /> */}
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="editIcon" viewBox="0 0 16 16">
+                                      <path d="M13.485 1.929a1 1 0 0 1 0 1.414L6.414 10.414l-3.9-3.9a1 1 0 1 1 1.414-1.414l2.485 2.485 6.657-6.657a1 1 0 0 1 1.414 0z" />
+                                    </svg>
+
                                   </button>
-                                  <button onClick={() => setEditingComment({})}>
-                                    <TbPencilCancel />
+                                  <button className="cancel-btn-comment" onClick={() => setEditingComment({})}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="icon" viewBox="0 0 16 16">
+                                      <path d="M2.5 2.5l11 11m0-11l-11 11" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                                    </svg>
                                   </button>
                                 </>
                               ) : (
                                 <>
-                                  <button
+                                  <button className="edit-btn-comment"
                                     onClick={() =>
                                       setEditingComment({ id: comment.id, content: comment.content })
                                     }
                                   >
-                                    <GrUpdate />
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="editIcon" viewBox="0 0 16 16">
+                                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706l-1.292 1.292-2.148-2.148L13.354.5a.5.5 0 0 1 .707 0l1.44 1.44zM1 13.5V16h2.5l9.255-9.255-2.148-2.148L1 13.5z" />
+                                    </svg>
+
+
                                   </button>
-                                  <button onClick={() => handleDeleteComment(post.id, comment.id)}>
-                                    <MdDelete />
+
+
+
+                                  <button className="delete-btn-comment" onClick={() => handleDeleteComment(post.id, comment.id)}>
+
+                                    <svg viewBox="0 0 15 17.5" height="17.5" width="15" xmlns="http://www.w3.org/2000/svg" class="icon">
+                                      <path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"></path>
+                                    </svg>
                                   </button>
                                 </>
                               )}
