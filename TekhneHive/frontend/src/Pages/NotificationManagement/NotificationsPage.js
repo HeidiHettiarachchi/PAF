3530b89f81a8976ba71d7f0d3e79d0c0f1df6d96
Chamudi <it22343048@my.sliit.lk>
2025-05-08 07:00:00 +0530

    Notification page bug fix

--- TekhneHive/frontend/src/Pages/NotificationManagement/NotificationsPage.js
+++ TekhneHive/frontend/src/Pages/NotificationManagement/NotificationsPage.js
@@ -10,17 +10,20 @@ function NotificationsPage() {
   const userId = localStorage.getItem('userID');
 
 
-  const fetchNotifications = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8080/notifications/${userId}`);
-      console.log('API Response is recorded:', response.data); 
-      setNotifications(response.data);
-    } catch (error) {
-      console.error('Error fetching the notifications:', error);
-    }
-  };
+
 
   useEffect(() => {
+    
+    const fetchNotifications = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/notifications/${userId}`);
+        console.log('API Response is recorded:', response.data); 
+        setNotifications(response.data);
+      } catch (error) {
+        console.error('Error fetching the notifications:', error);
+      }
+    };
+
     if (userId) {
       fetchNotifications();
     } else {
